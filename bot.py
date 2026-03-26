c8d6adcc7479932c35c6a0b428774a27c516ecd0
Melnik-Mot <02nov017@gmail.com>
2026-03-26 15:27:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
 
-TOKEN = 8666665014:AAEQbHOF-kZvno9PbzVZ2hlnVnATJrydvcs
+TOKEN = "8666665014:AAEQbHOF-kZvno9PbzVZ2hlnVnATJrydvcs"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -15,5 +15,5 @@ async def start_handler(message: Message):
 async def main():
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__== "__main__":
     asyncio.run(main())
