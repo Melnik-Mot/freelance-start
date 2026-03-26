b312a83875fd0fa77ea85aeac875dc9c7d4057a4
Melnik-Mot <02nov017@gmail.com>
2026-03-26 17:41:07 +0300

    Update calculatorbot.py

--- calculatorbot.py
+++ calculatorbot.py
@@ -1,1 +1,33 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
 
+TOKEN = "8747656943:AAEj5f7xuVzrqPu24YUucoYtN6Vjr-J_mCU"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Привет! Я калькулятор-бот 🤖\n"
+                         "Просто пришли мне математическое выражение, например:\n"
+                         "`2+2*3`")
+
+@dp.message()
+async def calculate(message: Message):
+    lines = message.text.splitlines()  # Разделяем текст по строкам
+    results = []
+    for line in lines:
+        try:
+            result = eval(line)  # вычисляем каждую строку
+            results.append(f"{line} = {result}")
+        except Exception:
+            results.append(f"{line} → ошибка!")
+    await message.answer("\n".join(results))
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
